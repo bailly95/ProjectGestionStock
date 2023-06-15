22e3ad3989dbb325359d5ba028d1f10ac7f2d877
bailly95 <bailly.g@hotmail.fr>
2023-06-15 14:22:17 +0200

    first

---
+++ src/main/java/fr/bailly/ProjetGestionStock/ProjetGestionStockApplication.java
@@ -0,0 +1,13 @@
+package fr.bailly.ProjetGestionStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGestionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetGestionStockApplication.class, args);
+	}
+
+}
