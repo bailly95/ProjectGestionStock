e35ce61e499e12cd573b026c4deb490912cbcfe9
bailly95 <bailly.g@hotmail.fr>
2023-06-15 14:50:17 +0200

    creation entities

---
+++ src/main/java/fr/bailly/ProjetGestionStock/model/Roles.java
@@ -0,0 +1,15 @@
+package fr.bailly.ProjetGestionStock.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Roles extends AbstractEntity{
+}
